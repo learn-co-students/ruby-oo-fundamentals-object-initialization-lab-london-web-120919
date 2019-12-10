af0268d87fc1aadd2bf14dbf9e4445533e8bbdbe
santiago molinari <santomolinari@gmail.com>
2019-12-10 16:43:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+    end
+
+    def print_dog_breed
+        puts "The dog's breed is #{@breed}"
+    end
+
+end
+
+
